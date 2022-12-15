d5a92dae5ed235a31a069f1a83974470d22e1a4b
Danish Javed <99647751+danishjavedcodes@users.noreply.github.com>
2022-12-15 19:44:09 +0500

    Change phone number data type because normal integer cannot store upto 14 numbers

--- sqlquries.sql
+++ sqlquries.sql
@@ -1,21 +1,27 @@
--- CREATE TABLE adminusers (f_name varchar not null,l_name varchar not null,user_id integer PRIMARY KEY not null,user_password varcharnot null,e_mail varcharnot null,phone_number integernot null,branch_code integernot null,user_type varcharnot null)
--- CREATE FUNCTION CHECK_PASS(U_ID INT, PASS VARCHAR)
--- RETURNS BOOLEAN AS $$
--- DECLARE 
--- 	CHECK_PASS BOOLEAN;
--- BEGIN
--- 	SELECT (user_password = $2) into CHECK_PASS
--- 	FROM adminusers
--- 	where user_id = $1;
--- 	return CHECK_PASS;
--- END;
--- $$  LANGUAGE plpgsql
+create table adminusers(f_name varchar not null,
+						l_name varchar,
+						user_id integer primary key not null,
+						user_password varchar not null,
+						e_mail varchar not null,
+						phone_number numeric (14) not null,
+						branch_code integer not null,
+						user_type varchar not null);
+CREATE FUNCTION CHECK_PASS(U_ID INT, PASS VARCHAR)
+RETURNS BOOLEAN AS $$
+DECLARE 
+	CHECK_PASS BOOLEAN;
+BEGIN
+	SELECT (user_password = $2) into CHECK_PASS
+	FROM adminusers
+	where user_id = $1;
+	return CHECK_PASS;
+END;
+$$  LANGUAGE plpgsql
 
--- select * from adminusers
+select * from adminusers
 
--- CREATE FUNCTION create_Adminuser(f_name varchar,l_name varchar,user_id integer,user_password varchar,e_mail varchar,phone_number integer,branch_code integer,user_type varchar)
--- RETURNS void as $$
--- BEGIN
--- 	insert into adminusers values ($1,$2,$3,$4,$5,$6,$7,$8);
--- END;
--- $$  LANGUAGE plpgsql
+CREATE FUNCTION create_Adminuser(f_name,l_name,user_id,user_password,e_mail,phone_number,branch_code,user_type)
+BEGIN
+	insert into adminusers values ($1,$2,$3,$4,$5,$6,$7,$8)
+END;
+select * from adminusers
