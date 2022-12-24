dc8bbb04f32b1faf015fae6d22ceab7a7a7f88f8
Danish Javed <99647751+danishjavedcodes@users.noreply.github.com>
2022-12-25 00:05:36 +0500

    use psycopg2
    use psycopg2 to execute raw quries

--- main.py
+++ main.py
@@ -2,42 +2,27 @@ from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+import psycopg2
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:sys@localhost/Wall-Street-Admin'
-db=SQLAlchemy(app)
+
+
 @app.route('/', methods=['GET', "POST"])
 def index():
     return render_template('index.html')
 
-Base = declarative_base()
-metadata = Base.metadata
+
+conn = psycopg2.connect(
+    database="Wall-Street-Admin",
+    user="postgres",
+    password="sys",
+    host="localhost",
+    port="5432"
+)
 
 
-
-class Adminuser(Base):
-    __tablename__ = 'adminusers'
-
-    f_name = Column(String, nullable=False)
-    l_name = Column(String)
-    user_id = Column(Integer, primary_key=True)
-    user_password = Column(String, nullable=False)
-    e_mail = Column(String, nullable=False)
-    phone_number = Column(Integer, nullable=False)
-    branch_code = Column(Integer, nullable=False)
-    user_type = Column(String, nullable=False)
-    def __init__(self, f_name, l_name, user_id, user_password, e_mail, phone_number, branch_code, user_type):
-        self.f_name = f_name
-        self.l_name = l_name
-        self.user_id = user_id
-        self.e_mail = e_mail
-        self.user_password = user_password
-        self.phone_number = phone_number
-        self.branch_code = branch_code
-        self.user_type = user_type
-    
-@app.route ('/submit', methods=['GET', 'POST'])
+@app.route('/submit', methods=['GET', 'POST'])
 def submit():
     if request.method == 'POST':
         f_name = request.form['f_name']
@@ -48,9 +33,27 @@ def submit():
         phone_number = request.form['phone_number']
         branch_code = request.form['branch_code']
         user_type = request.form['user_type']
-        newuser = Adminuser(f_name, l_name, user_id, user_password, e_mail, phone_number, branch_code, user_type)
-        db.session.add(newuser)
-        db.session.commit()
+        query = "'" + f_name + "'" + "," + "'" + l_name + "'" + "," + str(user_id) + "," + "'" + user_password + "'" + "," + "'" + e_mail + "'" + "," + str(
+            phone_number) + "," + str(branch_code) + "," + "'" + user_type + "'"
+        cur = conn.cursor()
+        cur.execute(
+            "INSERT INTO adminusers (f_name,l_name,user_id,user_password,e_mail,phone_number,branch_code,user_type) values (" + query + ")")
+
+        conn.commit()
+        cur = conn.cursor()
+        cur.execute("ROLLBACK")
+        conn.commit()
+        cur.execute("SELECT * FROM adminusers")
+        rows = cur.fetchall()
+        for row in rows:
+            print('First Name ', row[0])
+            print('Last Name ', row[1])
+            print('UserName ', row[2])
+            print('Password ', row[3])
+            print('EMAIL ', row[4])
+            print('Phone number ', row[5])
+            print('Branch ', row[6])
+            print('Type ', row[7])
     return render_template('index.html')
 
 
